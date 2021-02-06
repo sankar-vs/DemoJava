a2c9de7331090aee0b736517b36b1dc550766156
Sankar Vijaya Selvakumar <vs.sankar95@gmail.com>
2021-02-06 10:18:35 +0530

    ADD.prints UC1 code is added

--- HelloJava.java
+++ HelloJava.java
@@ -1,5 +1,10 @@
 public class HelloJava{
         public static void main(String[] args){
-                System.out.println("Hello world");
+                int a,b,c;
+		a=10;
+		b=15;
+		c=a+b;
+		System.out.println("UC1 code is added");
+		System.out.println("Hello world");
         }
 }

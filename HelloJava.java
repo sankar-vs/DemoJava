d4e745e5cdaaf053ed7328ff58bdffe7863e263e
Sankar Vijaya Selvakumar <57132054+sankar-vs@users.noreply.github.com>
2021-02-06 17:27:45 +0530

    [Sankar]DELETED.Removed the unused variables

--- HelloJava.java
+++ HelloJava.java
@@ -1,10 +1,6 @@
 public class HelloJava{
         public static void main(String[] args){
-                int a,b,c;
-		a=10;
-		b=15;
-		c=a+b;
-		System.out.println("UC1 code is added");
+                System.out.println("UC1 code is added");
 		System.out.println("Hello world");
         }
 }
